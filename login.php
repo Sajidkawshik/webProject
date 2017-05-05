11febd9199a029b92767d0206c2dd397d4fe9ad4
uprokash <uprokash@gmail.com>
2017-05-06 00:24:50 +0600

    uchchhwas6

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+	$conn=mysqli_connect('localhost','root',"","webpro");
+	if($conn->connect_errno>0)
+		echo "not connected";
+	if(isset($_POST["uname"], $_POST["psw"])) 
+    {     
+
+        $user_name = $_POST["uname"]; 
+        $password = $_POST["psw"]; 
+		
+		$sql= "SELECT * FROM admin WHERE (username = '".$user_name."' AND  password = '".$password."');";
+		$result1 = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result1) == 1 )
+        { 
+			$row = mysqli_fetch_array($result1);
+			session_start();
+            $_SESSION["login_status"]=true; 
+            $_SESSION["user_id"] = $row['username'];
+			header('Location: display_du_requests.php');
+        }
+        else
+        {
+            echo 'The username or password is incorrect!';
+        }
+	}
+	//mysqli_close($conn);
+?>
