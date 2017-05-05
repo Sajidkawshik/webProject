da0198185811d56481ad792121e09f464ec5070a
uprokash <uprokash@gmail.com>
2017-05-06 01:45:48 +0600

    uchchhwas6

---
+++ myqrcode/temporary.php
@@ -0,0 +1,22 @@
+<?php 
+ include('phpqrcode/qrlib.php'); 
+ define('EXAMPLE_TMP_SERVERPATH', dirname(__FILE__).'/temp/');
+ define('EXAMPLE_TMP_URLRELPATH', 'phpqrcode/temp/');
+ $tempDir = EXAMPLE_TMP_SERVERPATH;
+ 
+$fname=$_GET['fname'];
+$lname=$_GET['lname'];
+$email=$_GET['email'];
+$username=$fname.' '.$lname;
+$dept=$_GET['dept'];
+$con_add=$_GET['con_add'];
+$blood=$_GET['blood'];
+//echo $blood;
+$contact=$_GET['contact'];
+//echo $contact;
+$license=$_GET['license'];
+$car=$_GET['car'];
+$codeContents='Name:- '.$fname.' '.$lname.' '.'Adress:- '.$con_add.' '.'Mobile:- '.$contact.' '.'License NO:- '.$license.' '.'Car NO:- '.$car;
+QRcode::png($codeContents, $tempDir.$license.'.png', QR_ECLEVEL_L, 3);
+header("Location: http://localhost/webProject/create_pdf/demopdf.php?username=$username&dept=$dept&car=$car&license=$license&email=$email");
+?>
