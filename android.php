fbb222b786ca8e8588ce70a07539ae0596dd2826
uprokash <uprokash@gmail.com>
2017-05-06 18:57:05 +0600

    uchchhwas6

--- android.php
+++ android.php
@@ -11,18 +11,22 @@ $db_name = 'webpro'; // Database Name
 $conn=mysqli_connect('localhost','duvehicle','duvehicle','duvehicle');
 $tok=strtok($name,":- ");
 while ($tok !== false) {
-    echo "Word=$tok<br />";
+    //echo "Word=$tok<br />";
     $tok = strtok(":- ");
 }
-echo $tok;
-$sql = "SELECT * FROM du WHERE car_no='$tok'";
- 
- 
+//echo $tok;
+$sql = "SELECT * FROM reg_du WHERE car_no='$tok'";
+$sql1 = "SELECT * FROM reg_non_du WHERE car_no='$tok'";
+
 	    $res = mysqli_query($conn,$sql);
+		$res1 = mysqli_query($conn,$sql1);
 		$row = mysqli_fetch_array($res);
+		$row1 = mysqli_fetch_array($res1);
 		//echo $row[1];
 		if( $row[0] > 0 )
-		 echo "Verified user";
+		 echo "Verified DU user";
+		else if($row1[0] > 0)
+		 echo "Verified Non DU user";
 		else
-		 echo "faltu user";		
+			echo "Invalid user";
 ?> 
