0c974193b56329a98c4ea7c0aefb80d99e1336ec
uprokash <uprokash@gmail.com>
2017-05-06 23:43:36 +0600

    uchchhwas6

--- create_pdf/demopdf.php
+++ create_pdf/demopdf.php
@@ -8,18 +8,29 @@ $email=isset($_GET['email']) ? $_GET['email'] : 'faltunoman2017@gmail.com';
 
 $pdf=new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',18);
-$pdf->Cell(10,100,'Name:- '.$username);
-$pdf->Cell(10,120,'Department:- '.$dept);
-$pdf->Cell(10,140,'Car Number:- '.$car);
-
 $image1='../myqrcode/temp/'.$license.'.png';
 $image2='../myqrcode/temp/DU.png';
-$pdf->Cell( 40, 40, $pdf->Image($image2, 100, 10, 33.78), 0, 0, 'L', false );
-$pdf->Cell( 40, 40, $pdf->Image($image1, 100, 100, 33.78), 0, 0, 'L', false );
-//$pdf->Cell(40,10,'This is a faltu');
-$path='../save_pdf/'.$license.'.pdf';
-$pdf->output($path,'F');
+
+
+
+$pdf->SetY(70);
+$pdf->SetFont('Times','B',18);
+$pdf->Cell(0,10,'Name: '.$username,0,0,'C');
+$pdf->SetY(78);
+$pdf->Cell(0,10,'Department: '.$dept,0,0,'C');
+$pdf->SetY(86);
+$pdf->Cell(0,10,'Car Number:- '.$car,0,0,'C');
+
+$pdf->Cell( 0, 10, $pdf->Image($image1, 89, 100, 33.78), 0, 0, 'L', false );
+
+$pdf->Cell( 40, 40, $pdf->Image($image2, 89, 22, 33.78), 0, 0, 'L', false );
+$pdf->SetLineWidth(1.5);
+$pdf->Line(20, 12, 210-20, 12);
+$pdf->Line(20, 140, 210-20, 140);
+$pdf->Line(20, 12, 20, 140);
+$pdf->Line(210-20, 12, 210-20, 140);
+
 $path='save_pdf/'.$license.'.pdf';
+$pdf->output($path,'F');
 header("Location: http://csedu.cf/duvehicle/swift.php?path=$path&email=$email");
 ?>
--- create_pdf/demopdf1.php
+++ create_pdf/demopdf1.php
@@ -1,23 +1,35 @@
 <?php
 require('fpdf.php');
 $username = isset($_GET['username']) ? $_GET['username'] : 'default';
+$dept = isset($_GET['dept']) ? $_GET['dept'] : 'CSE';
 $car = isset($_GET['car']) ? $_GET['car'] : 'ka-1000';
 $license=isset($_GET['license']) ? $_GET['license'] : 'license';
 $email=isset($_GET['email']) ? $_GET['email'] : 'faltunoman2017@gmail.com';
 
 $pdf=new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',18);
-$pdf->Cell(10,100,'Name:- '.$username);
-$pdf->Cell(10,140,'Car Number:- '.$car);
-
 $image1='../myqrcode/temp/'.$license.'.png';
 $image2='../myqrcode/temp/DU.png';
-$pdf->Cell( 40, 40, $pdf->Image($image2, 100, 10, 33.78), 0, 0, 'L', false );
-$pdf->Cell( 40, 40, $pdf->Image($image1, 100, 100, 33.78), 0, 0, 'L', false );
-//$pdf->Cell(40,10,'This is a faltu');
-$path='../save_pdf/'.$license.'.pdf';
-$pdf->output($path,'F');
+
+
+
+$pdf->SetY(70);
+$pdf->SetFont('Times','B',18);
+$pdf->Cell(0,10,'Name: '.$username,0,0,'C');
+$pdf->SetY(78);
+$pdf->Cell(0,10,'Car Number:- '.$car,0,0,'C');
+
+
+$pdf->Cell( 0, 10, $pdf->Image($image1, 89, 100, 33.78), 0, 0, 'L', false );
+
+$pdf->Cell( 40, 40, $pdf->Image($image2, 89, 22, 33.78), 0, 0, 'L', false );
+$pdf->SetLineWidth(1.5);
+$pdf->Line(20, 12, 210-20, 12);
+$pdf->Line(20, 140, 210-20, 140);
+$pdf->Line(20, 12, 20, 140);
+$pdf->Line(210-20, 12, 210-20, 140);
+
 $path='save_pdf/'.$license.'.pdf';
+$pdf->output($path,'F');
 header("Location: http://csedu.cf/duvehicle/swift.php?path=$path&email=$email");
 ?>
